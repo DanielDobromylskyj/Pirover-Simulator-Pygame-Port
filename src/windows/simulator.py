75f6dbd14bcf7eb99c60b90f088d19bb784f3b8d
DanielDobromylskyj <90842024+DanielDobromylskyj@users.noreply.github.com>
2024-10-28 21:01:31 +0000

    Added a World class

--- src/windows/simulator.py
+++ src/windows/simulator.py
@@ -1,16 +1,15 @@
 import pygame
 
 from src.robots.initio import Initio
+from src.world import World, get_window_size
 
 pygame.init()
-
-
 
 
 class Simulator:
     def __init__(self, world_file="default.xml", selected_robot="Initio", window_size=None, target_fps=60):
         if window_size is None:
-            window_size = pygame.display.get_desktop_sizes()[0]
+            window_size = get_window_size(world_file)
 
         self.window = pygame.display.set_mode(window_size)
         self.clock = pygame.time.Clock()
@@ -18,10 +17,7 @@ class Simulator:
         self.running = False
         self.target_fps = target_fps
 
-        self.sonar_map = None  # NOT IMPLEMENTED - todo
-        self.line_map_sprite = None # NOT IMPLEMENTED - todo
-        self.static_objects = None # NOT IMPLEMENTED - todo
-
+        self.world = World(self.window, world_file)
         self.robot = self.load_robot(selected_robot)
 
     def load_robot(self, selected):
@@ -29,9 +25,9 @@ class Simulator:
             case "Initio":
                 return Initio(
                     self.window,
-                    self.sonar_map,
-                    self.line_map_sprite,
-                    self.static_objects,
+                    self.world.sonar_map,
+                    self.world.line_map_sprite,
+                    self.world.static_objects,
                     self.window.get_width(),
                     self.window.get_height()
                 )
@@ -43,10 +39,11 @@ class Simulator:
                 raise NameError(f"No Robot With Name '{selected}'")
 
     def render(self):
-        pass
+        self.world.render()
+        self.robot.render()
 
     def update(self, delta_time):
-        pass
+        self.robot.update(delta_time, self)
 
     def run(self):
         self.running = True
